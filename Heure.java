1c12778a10b6644e98c007807f811f8ce5a3c7fc
Benach <imane.benach.sio@gmail.com>
2018-09-14 09:34:38 +0200

    Classe Heure ["réussi"]

--- Heure.java
+++ Heure.java
@@ -3,6 +3,18 @@ public class Heure {
 	private int heures ;
 	private int minutes ;
 	
+	public void Heure (int, int) {
+		this.heure = heures ;
+		this.minutes = minutes ;	
+	}
+	
+	public getHeure(){
+		return this.heures;
+	}
+	
+	public getMinutes(){
+		return this.minutes;	
+	}
 	
 	
 }
